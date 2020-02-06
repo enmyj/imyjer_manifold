5459848fbbf089b1fb6626e5baeb65aad5984c46
Ian <imyjer@gmail.com>
2020-02-05 22:35:30 -0500

    simplified API gateway setup

--- terraform/lambda_api.tf
+++ terraform/lambda_api.tf
@@ -79,118 +79,52 @@ resource "aws_lambda_permission" "apigw" {
   function_name = "${aws_lambda_function.manifold_ian_api.arn}"
   principal     = "apigateway.amazonaws.com"
 
-  source_arn = "${aws_api_gateway_rest_api.manifold_ian_gw.execution_arn}/*/*"
+  source_arn = "${aws_api_gateway_rest_api.imyjer_manifold_api.execution_arn}/*/*"
 }
 
 ############################################
 # API GATEWAY
 ############################################
 
-resource "aws_api_gateway_rest_api" "manifold_ian_gw" {
-  name        = "${var.api_name}"
-  description = "created by terraform"
+# api gateway parent object
+resource "aws_api_gateway_rest_api" "imyjer_manifold_api" {
+  name = "Ian Manifold API Gateway"
+  description = "Ian Manifold API Gateway"
 }
 
-resource "aws_api_gateway_resource" "proxy" {
-  rest_api_id = "${aws_api_gateway_rest_api.manifold_ian_gw.id}"
-  parent_id   = "${aws_api_gateway_rest_api.manifold_ian_gw.root_resource_id}"
-  path_part   = "${var.api_path_part}"
+# path resource
+resource "aws_api_gateway_resource" "imyjer_manifold_gw_resource" {
+  rest_api_id = "${aws_api_gateway_rest_api.imyjer_manifold_api.id}"
+  parent_id = "${aws_api_gateway_rest_api.imyjer_manifold_api.root_resource_id}"
+  path_part = "${var.api_path_part}"
 }
 
-resource "aws_api_gateway_method" "options_method" {
-  rest_api_id   = "${aws_api_gateway_rest_api.manifold_ian_gw.id}"
-  resource_id   = "${aws_api_gateway_resource.proxy.id}"
-  http_method   = "OPTIONS"
+# POST method on path resource above ^^
+resource "aws_api_gateway_method" "imyjer_manifold_api_method" {
+  rest_api_id = "${aws_api_gateway_rest_api.imyjer_manifold_api.id}"
+  resource_id = "${aws_api_gateway_resource.imyjer_manifold_gw_resource.id}"
+  http_method = "POST"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_method_response" "options_200" {
-  rest_api_id = "${aws_api_gateway_rest_api.manifold_ian_gw.id}"
-  resource_id = "${aws_api_gateway_resource.proxy.id}"
-  http_method = "${aws_api_gateway_method.options_method.http_method}"
-  status_code = "200"
-
-  response_models {
-    "application/json" = "Empty"
-  }
-
-  response_parameters {
-    "method.response.header.Access-Control-Allow-Headers" = true
-    "method.response.header.Access-Control-Allow-Methods" = true
-    "method.response.header.Access-Control-Allow-Origin"  = true
-  }
-
-  depends_on = ["aws_api_gateway_method.options_method"]
-}
-
-resource "aws_api_gateway_integration" "options_integration" {
-  rest_api_id = "${aws_api_gateway_rest_api.manifold_ian_gw.id}"
-  resource_id = "${aws_api_gateway_resource.proxy.id}"
-  http_method = "${aws_api_gateway_method.options_method.http_method}"
-  type        = "MOCK"
-
-  request_templates {
-    "application/json" = "{ \"statusCode\": 200 }"
-  }
-
-  depends_on = ["aws_api_gateway_method.options_method"]
-}
-
-resource "aws_api_gateway_integration_response" "options_integration_response" {
-  rest_api_id = "${aws_api_gateway_rest_api.manifold_ian_gw.id}"
-  resource_id = "${aws_api_gateway_resource.proxy.id}"
-  http_method = "${aws_api_gateway_method.options_method.http_method}"
-  status_code = "${aws_api_gateway_method_response.options_200.status_code}"
-
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
-    "method.response.header.Access-Control-Allow-Methods" = "'DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT'"
-    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
-  }
-
-  depends_on = ["aws_api_gateway_method_response.options_200"]
-}
-
-resource "aws_api_gateway_method" "proxy" {
-  rest_api_id   = "${aws_api_gateway_rest_api.manifold_ian_gw.id}"
-  resource_id   = "${aws_api_gateway_resource.proxy.id}"
-  http_method   = "ANY"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_method_response" "200" {
-  rest_api_id = "${aws_api_gateway_rest_api.manifold_ian_gw.id}"
-  resource_id = "${aws_api_gateway_resource.proxy.id}"
-  http_method = "${aws_api_gateway_method.proxy.http_method}"
-  status_code = "200"
-
-  response_models = {
-    "application/json" = "Empty"
-  }
-
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Origin" = true
-  }
-
-  depends_on = ["aws_api_gateway_method.proxy"]
-}
-
+# integration with lambda
 resource "aws_api_gateway_integration" "lambda" {
-  rest_api_id = "${aws_api_gateway_rest_api.manifold_ian_gw.id}"
-  resource_id = "${aws_api_gateway_method.proxy.resource_id}"
-  http_method = "${aws_api_gateway_method.proxy.http_method}"
+  rest_api_id = "${aws_api_gateway_rest_api.imyjer_manifold_api.id}"
+  resource_id = "${aws_api_gateway_resource.imyjer_manifold_gw_resource.id}"
+  http_method = "${aws_api_gateway_method.imyjer_manifold_api_method.http_method}"
 
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = "${aws_lambda_function.manifold_ian_api.invoke_arn}"
-  depends_on              = ["aws_api_gateway_method.proxy", "aws_lambda_function.manifold_ian_api"]
 }
 
+# deployment
 resource "aws_api_gateway_deployment" "gw_deploy" {
   depends_on = [
     "aws_api_gateway_integration.lambda",
+    "aws_api_gateway_method.imyjer_manifold_api_method"
   ]
 
-  rest_api_id = "${aws_api_gateway_rest_api.manifold_ian_gw.id}"
+  rest_api_id = "${aws_api_gateway_rest_api.imyjer_manifold_api.id}"
   stage_name  = "prod"
 }
